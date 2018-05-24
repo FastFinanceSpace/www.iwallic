3936814f2e6f7b3ca54f20e3beba1747d3d568b5
haiwulei <1021391538@qq.com>
2018-05-24 16:59:29 +0800

    change img

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -9,19 +9,13 @@ $(document).ready(function() {
     $('.dropbtn span').text('English');
   }
   $('.dropbtn').click(function() {
-    $('.dropdown-content').show();
+    $('.dropdown-content').fadeIn('normal');
   }) 
   $('body').click(function(e) {
     e = e || window.event;
-    var obj =e.target || e.srcElement;
+    var obj = e.target || e.srcElement;
     if($(obj).closest(".dropbtn").length <=0){//点击的区域如果没有id为explain的标签，就将explain收起隐藏
-      $('.dropdown-content').hide();  
+      $('.dropdown-content').fadeOut('normal');  
     }
-  })
-  $('.lang-zh').click(function() {
-    window.location.href = '/tw/index.html';
-  })
-  $('.lang-en').click(function() {
-    window.location.href = '/en/index.html';
   })
 });
