0ecdb2eb6bde1870694dcaabec7f01cf29e8ef63
haiwulei <1021391538@qq.com>
2018-05-24 15:56:30 +0800

    version0.0.2

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -10,128 +10,128 @@ const NamedModulesPlugin = require('webpack/lib/NamedModulesPlugin');
 const EvalSourceMapDevToolPlugin = require('webpack/lib/EvalSourceMapDevToolPlugin');
 
 module.exports = {
-    entry: {
-        'main': './src/scripts/site.js'
-    },
-    resolve: {
-        extensions: ['.js', '.json']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                  fallback: 'style-loader',
-                  use: 'css-loader'
-                }),
-                include: ['./src/styles']
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                  {
-                    loader: 'style-loader'
-                  },
-                  {
-                    loader: 'css-loader'
-                  },
-                  {
-                    loader: 'sass-loader'
-                  }
-                ]
-            },
-            {
-                test: /\.(jpg|png|gif|pdf)$/,
-                use: 'file-loader'
-            },
-            {
-              test: /\.(eot|woff2?|svg|ttf)([\?]?.*)$/,
-              use: 'file-loader'
-            }
+  entry: {
+    'main': './src/scripts/site.js'
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        }),
+        include: ['./src/styles']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
         ]
-    },
-    plugins: [
-        new EvalSourceMapDevToolPlugin({
-            moduleFilenameTemplate: '[resource-path]',
-            sourceRoot: 'webpack:///'
-        }),
-        new CommonsChunkPlugin({
-            name: 'main',
-            async: 'common',
-            children: true,
-            minChunks: 2
-        }),
-        new ExtractTextPlugin('[name].[contenthash].css'),
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.css$/g,
-            cssProcessor: require('cssnano'),
-            cssProcessorOptions: { discardComments: {removeAll: true } },
-            canPrint: true
-        }),
-        new CopyWebpackPlugin([
-            { from: 'src/assets', to: 'assets' },
-        ]),
-        new HtmlWebpackPlugin({
-            template: 'src/sites/en/index.html',
-            inject: 'body',
-            xhtml: true,
-            metadata: {
-                isDevServer: true
-            },
-            minify: {
-              caseSensitive: true,
-              collapseWhitespace: true,
-              keepClosingSlash: true
-            },
-            chunks: 'all',
-            excludeChunks: [],
-            filename: 'index.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: 'src/sites/en/index.html',
-            inject: 'body',
-            xhtml: true,
-            metadata: {
-                isDevServer: true
-            },
-            minify: {
-              caseSensitive: true,
-              collapseWhitespace: true,
-              keepClosingSlash: true
-            },
-            chunks: 'all',
-            excludeChunks: [],
-            filename: 'en/index.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: 'src/sites/tw/index.html',
-            inject: 'body',
-            xhtml: true,
-            metadata: {
-                isDevServer: true
-            },
-            minify: {
-              caseSensitive: true,
-              collapseWhitespace: true,
-              keepClosingSlash: true
-            },
-            chunks: 'all',
-            excludeChunks: [],
-            filename: 'tw/index.html'
-        })
-    ],
-    devServer: {
-        port: 3002,
-        host: '127.0.0.1',
-        historyApiFallback: true,
-        watchOptions: {
-          ignored: /node_modules/
-        }
-    },
-    output: {
-        path: path.resolve(__dirname, '../dist'),
-        filename: '[name].bundle.js',
-        sourceMapFilename: '[file].map',
-        chunkFilename: '[name].chunk.js'
+      },
+      {
+        test: /\.(jpg|png|gif|pdf)$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.(eot|woff2?|svg|ttf)([\?]?.*)$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new EvalSourceMapDevToolPlugin({
+      moduleFilenameTemplate: '[resource-path]',
+      sourceRoot: 'webpack:///'
+    }),
+    new CommonsChunkPlugin({
+      name: 'main',
+      async: 'common',
+      children: true,
+      minChunks: 2
+    }),
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: {removeAll: true } },
+      canPrint: true
+    }),
+    new CopyWebpackPlugin([
+      { from: 'src/assets', to: 'assets' },
+    ]),
+    new HtmlWebpackPlugin({
+      template: 'src/sites/en/index.html',
+      inject: 'body',
+      xhtml: true,
+      metadata: {
+        isDevServer: true
+      },
+      minify: {
+        caseSensitive: true,
+        collapseWhitespace: true,
+        keepClosingSlash: true
+      },
+      chunks: 'all',
+      excludeChunks: [],
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/sites/en/index.html',
+      inject: 'body',
+      xhtml: true,
+      metadata: {
+        isDevServer: true
+      },
+      minify: {
+        caseSensitive: true,
+        collapseWhitespace: true,
+        keepClosingSlash: true
+      },
+      chunks: 'all',
+      excludeChunks: [],
+      filename: 'en/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/sites/tw/index.html',
+      inject: 'body',
+      xhtml: true,
+      metadata: {
+        isDevServer: true
+      },
+      minify: {
+        caseSensitive: true,
+        collapseWhitespace: true,
+        keepClosingSlash: true
+      },
+      chunks: 'all',
+      excludeChunks: [],
+      filename: 'tw/index.html'
+    })
+  ],
+  devServer: {
+    port: 3002,
+    host: '0.0.0.0',
+    historyApiFallback: true,
+    watchOptions: {
+      ignored: /node_modules/
     }
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[file].map',
+    chunkFilename: '[name].chunk.js'
+  }
 };
--- src/scripts/site.js
+++ src/scripts/site.js
@@ -1,6 +1,27 @@
 import '../styles/global.scss';
+import '../styles/index.scss';
 import * as $ from 'jquery';
 
 $(document).ready(function() {
-    $('#iwallic').html('iWallic');
+  if (window.location.href.indexOf('tw') >= 0) {
+    $('.dropbtn span').text('中文');
+  } else {
+    $('.dropbtn span').text('English');
+  }
+  $('.dropbtn').click(function() {
+    $('.dropdown-content').show();
+  }) 
+  $('body').click(function(e) {
+    e = e || window.event;
+    var obj =e.target || e.srcElement;
+    if($(obj).closest(".dropbtn").length <=0){//点击的区域如果没有id为explain的标签，就将explain收起隐藏
+      $('.dropdown-content').hide();  
+    }
+  })
+  $('.lang-zh').click(function() {
+    window.location.href = '/tw/index.html';
+  })
+  $('.lang-en').click(function() {
+    window.location.href = '/en/index.html';
+  })
 });
