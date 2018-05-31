b471192b61e4595426ea346d454528b0ea32a2b9
haiwulei <1021391538@qq.com>
2018-05-31 12:44:06 +0800

    ios download link

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -18,16 +18,39 @@ $(document).ready(function() {
       $('.dropdown-content').fadeOut('normal');  
     }
   })
-  $('.btn-ios').on('click', function() {
-    if (window.location.href.indexOf('en') >= 0) {
-      alert('Go to the official channel to get test qualification and other information.');
-    } else {
-      alert('去官方渠道获取测试资格等消息。');
-    }
-  })
+  // $('.btn-ios').on('click', function() {
+  //   if (window.location.href.indexOf('en') >= 0) {
+  //     alert('Go to the official channel to get test qualification and other information.');
+  //   } else {
+  //     alert('去官方渠道获取测试资格等消息。');
+  //   }
+  // })
   $('.btn-android').on('click', function() {
     $.getJSON('../assets/config/version.json', function(data) {
       window.location.href = data.android;
     });
-  })
+  });
+  var x = 5;
+  var y = 15;
+  var title = '';
+  if (window.location.href.indexOf('en') >= 0) {
+    title = 'Go to the official channel to get test qualification and other information.';
+  } else {
+    title = '去官方渠道获取测试资格等消息。';
+  }
+  $(".download-tip").click(function(e) {
+    if (window.location.href.indexOf('en') >= 0) {
+      if ($('.download-tip').text() === 'iOS download') {
+        $('.download-tip').text('Ask iWallic team for testing');
+      } else {
+        $('.download-tip').text('iOS download');
+      }
+    } else {
+      if ($('.download-tip').text() === 'iOS 下载') {
+        $('.download-tip').text('联系官方参与公测');
+      } else {
+        $('.download-tip').text('iOS 下载');
+      }
+    }
+  });
 });
