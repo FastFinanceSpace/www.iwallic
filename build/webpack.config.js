76c00d2afcdae288933aee5ae492cbdeff5085a9
haiwulei <1021391538@qq.com>
2018-07-06 16:47:09 +0800

    change indentation 2 to 4, add version number

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -8,136 +8,136 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-  entry: {
-    'main': './src/scripts/site.js'
-  },
-  resolve: {
-    extensions: ['.js', '.json']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: ['css-loader', 'sass-loader']
-        })
-      },
-      {
-        test: /\.(jpg|png|gif|pdf)$/,
-        use: 'file-loader'
-      },
-      {
-        test: /\.(eot|woff2?|svg|ttf)([\?]?.*)$/,
-        use: 'file-loader'
-      }
-    ]
-},
-plugins: [
-    new CommonsChunkPlugin({
-      name: 'main',
-      async: 'common',
-      children: true,
-      minChunks: 2
-    }),
-    new ExtractTextPlugin('[name].[contenthash].css'),
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /\.css$/g,
-      cssProcessor: require('cssnano'),
-      cssProcessorOptions: { discardComments: {removeAll: true } },
-      canPrint: true
-    }),
-    new CopyWebpackPlugin([
-      { from: 'src/assets', to: 'assets' },
-    ]),
-    new HtmlWebpackPlugin({
-      template: 'src/assets/disclaimer/index.html',
-      inject: 'body',
-      xhtml: true,
-      metadata: {
-        isDevServer: false
-      },
-      minify: {
-        caseSensitive: true,
-        collapseWhitespace: true,
-        keepClosingSlash: true
-      },
-      chunks: 'all',
-      excludeChunks: [],
-      filename: 'assets/disclaimer/index.html'
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/sites/cn/index.html',
-      inject: 'body',
-      xhtml: true,
-      metadata: {
-        isDevServer: false
-      },
-      minify: {
-        caseSensitive: true,
-        collapseWhitespace: true,
-        keepClosingSlash: true
-      },
-      chunks: 'all',
-      excludeChunks: [],
-      filename: 'index.html'
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/sites/en/index.html',
-      inject: 'body',
-      xhtml: true,
-      metadata: {
-        isDevServer: false
-      },
-      minify: {
-        caseSensitive: true,
-        collapseWhitespace: true,
-        keepClosingSlash: true
-      },
-      chunks: 'all',
-      excludeChunks: [],
-      filename: 'en/index.html'
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/sites/cn/index.html',
-      inject: 'body',
-      xhtml: true,
-      metadata: {
-        isDevServer: false
-      },
-      minify: {
-        caseSensitive: true,
-        collapseWhitespace: true,
-        keepClosingSlash: true
-      },
-      chunks: 'all',
-      excludeChunks: [],
-      filename: 'cn/index.html'
-    }),
-    new UglifyJsPlugin({
-      sourceMap: true,
-      uglifyOptions: {
-        ecma: 5,
-        warnings: false,    // TODO verbose based on option?
-        ie8: false,
-        mangle: true,
-        compress: {
-          pure_getters: true, /* buildOptimizer */
-          // PURE comments work best with 3 passes.
-          // See https://github.com/webpack/webpack/issues/2899#issuecomment-317425926.
-          passes: 3
-        },
-        output: {
-          ascii_only: true,
-          comments: false
-        }
-      }
-    })
-  ],
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: '[name].[chunkhash].bundle.js',
-    sourceMapFilename: '[file].map',
-    chunkFilename: '[name].[chunkhash].chunk.js'
-  }
+	entry: {
+		'main': './src/scripts/site.js'
+	},
+	resolve: {
+		extensions: ['.js', '.json']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
+			},
+			{
+				test: /\.(jpg|png|gif|pdf)$/,
+				use: 'file-loader'
+			},
+			{
+				test: /\.(eot|woff2?|svg|ttf)([\?]?.*)$/,
+				use: 'file-loader'
+			}
+		]
+	},
+	plugins: [
+		new CommonsChunkPlugin({
+			name: 'main',
+			async: 'common',
+			children: true,
+			minChunks: 2
+		}),
+		new ExtractTextPlugin('[name].[contenthash].css'),
+		new OptimizeCssAssetsPlugin({
+			assetNameRegExp: /\.css$/g,
+			cssProcessor: require('cssnano'),
+			cssProcessorOptions: { discardComments: { removeAll: true } },
+			canPrint: true
+		}),
+		new CopyWebpackPlugin([
+			{ from: 'src/assets', to: 'assets' },
+		]),
+		new HtmlWebpackPlugin({
+			template: 'src/assets/disclaimer/index.html',
+			inject: 'body',
+			xhtml: true,
+			metadata: {
+				isDevServer: false
+			},
+			minify: {
+				caseSensitive: true,
+				collapseWhitespace: true,
+				keepClosingSlash: true
+			},
+			chunks: 'all',
+			excludeChunks: [],
+			filename: 'assets/disclaimer/index.html'
+		}),
+		new HtmlWebpackPlugin({
+			template: 'src/sites/cn/index.html',
+			inject: 'body',
+			xhtml: true,
+			metadata: {
+				isDevServer: false
+			},
+			minify: {
+				caseSensitive: true,
+				collapseWhitespace: true,
+				keepClosingSlash: true
+			},
+			chunks: 'all',
+			excludeChunks: [],
+			filename: 'index.html'
+		}),
+		new HtmlWebpackPlugin({
+			template: 'src/sites/en/index.html',
+			inject: 'body',
+			xhtml: true,
+			metadata: {
+				isDevServer: false
+			},
+			minify: {
+				caseSensitive: true,
+				collapseWhitespace: true,
+				keepClosingSlash: true
+			},
+			chunks: 'all',
+			excludeChunks: [],
+			filename: 'en/index.html'
+		}),
+		new HtmlWebpackPlugin({
+			template: 'src/sites/cn/index.html',
+			inject: 'body',
+			xhtml: true,
+			metadata: {
+				isDevServer: false
+			},
+			minify: {
+				caseSensitive: true,
+				collapseWhitespace: true,
+				keepClosingSlash: true
+			},
+			chunks: 'all',
+			excludeChunks: [],
+			filename: 'cn/index.html'
+		}),
+		new UglifyJsPlugin({
+			sourceMap: true,
+			uglifyOptions: {
+				ecma: 5,
+				warnings: false,    // TODO verbose based on option?
+				ie8: false,
+				mangle: true,
+				compress: {
+					pure_getters: true, /* buildOptimizer */
+					// PURE comments work best with 3 passes.
+					// See https://github.com/webpack/webpack/issues/2899#issuecomment-317425926.
+					passes: 3
+				},
+				output: {
+					ascii_only: true,
+					comments: false
+				}
+			}
+		})
+	],
+	output: {
+		path: path.resolve(__dirname, '../dist'),
+		filename: '[name].[chunkhash].bundle.js',
+		sourceMapFilename: '[file].map',
+		chunkFilename: '[name].[chunkhash].chunk.js'
+	}
 };
--- src/scripts/site.js
+++ src/scripts/site.js
@@ -3,105 +3,105 @@ import '../styles/global.scss';
 import '../styles/index.scss';
 
 $(document).ready(function() {
-  // article height
-  var articleHeight = window.innerHeight || document.body.clientHeight || document.documentElement.clientHeight;
-  var articleWidth = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
-  articleHeight -= 42;
-  if ($('.left-img').css('display') === 'block') {
-    articleHeight = articleHeight > 665 ? articleHeight : 665;
-  } else {
-    articleHeight = articleHeight > 665 ? articleHeight : 500;
-  }
-  $("article").css("height", articleHeight);
-  if (articleWidth > 930) {
-    $(".download").hide();
-    $(".pc-download").show();
-  } else {
-    $(".download").show();
-    $(".pc-download").hide();
-  }
+	// article height
+	var articleHeight = window.innerHeight || document.body.clientHeight || document.documentElement.clientHeight;
+	var articleWidth = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+	articleHeight -= 42;
+	if ($('.left-img').css('display') === 'block') {
+		articleHeight = articleHeight > 665 ? articleHeight : 665;
+	} else {
+		articleHeight = articleHeight > 665 ? articleHeight : 500;
+	}
+	$("article").css("height", articleHeight);
+	if (articleWidth > 930) {
+		$(".download").hide();
+		$(".pc-download").show();
+	} else {
+		$(".download").show();
+		$(".pc-download").hide();
+	}
 
-  $(window).on("resize",function(){
-    articleHeight = window.innerHeight || document.body.clientHeight || document.documentElement.clientHeight;
-    articleWidth = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
-    articleHeight -= 42;
-    if ($('.left-img').css('display') === 'block') {
-      articleHeight = articleHeight > 665 ? articleHeight : 665;
-    } else {
-      articleHeight = articleHeight > 700 ? articleHeight : 500;
-    }
-    $("article").css("height", articleHeight);
-    if (articleWidth > 930) {
-      $(".download").hide();
-      $(".pc-download").show();
-    } else {
-      $(".download").show();
-      $(".pc-download").hide();
-    }
-  });
+	$(window).on("resize",function(){
+		articleHeight = window.innerHeight || document.body.clientHeight || document.documentElement.clientHeight;
+		articleWidth = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+		articleHeight -= 42;
+		if ($('.left-img').css('display') === 'block') {
+			articleHeight = articleHeight > 665 ? articleHeight : 665;
+		} else {
+			articleHeight = articleHeight > 700 ? articleHeight : 500;
+		}
+		$("article").css("height", articleHeight);
+		if (articleWidth > 930) {
+			$(".download").hide();
+			$(".pc-download").show();
+		} else {
+			$(".download").show();
+			$(".pc-download").hide();
+		}
+	});
 
-  // toggle language
-  if (window.location.href.indexOf('en') >= 0) {
-    $('.dropbtn span').text('English');
-  } else {
-    $('.dropbtn span').text('中文简体');
-  }
-  $('.dropbtn').on('click', function() {
-    $('.dropdown-content').fadeIn('normal');
-  }) 
-  $('.dropbtn').on('touchstart', function() {
-    $('.dropdown-content').fadeIn('normal');
-  }) 
+	// toggle language
+	if (window.location.href.indexOf('en') >= 0) {
+		$('.dropbtn span').text('English');
+	} else {
+		$('.dropbtn span').text('中文简体');
+	}
+	$('.dropbtn').on('click', function() {
+		$('.dropdown-content').fadeIn('normal');
+	}) 
+	$('.dropbtn').on('touchstart', function() {
+		$('.dropdown-content').fadeIn('normal');
+	}) 
 
-  // toggle language dropcontent
-  $('body').on('click', function(e) {
-    e = e || window.event;
-    var obj = e.target || e.srcElement;
-    if($(obj).closest(".dropbtn").length <=0){ // If the clicked area has no id comment, it will hide the explain
-      $('.dropdown-content').fadeOut('normal');  
-    }
-  })
-  $('body').on('touchend', function(e) {
-    e = e || window.event;
-    var obj = e.target || e.srcElement;
-    if($(obj).closest(".dropbtn").length <=0){ // If the clicked area has no id comment, it will hide the explain
-      $('.dropdown-content').fadeOut('normal');  
-    }
-  })
+	// toggle language dropcontent
+	$('body').on('click', function(e) {
+		e = e || window.event;
+		var obj = e.target || e.srcElement;
+		if($(obj).closest(".dropbtn").length <=0){ // If the clicked area has no id comment, it will hide the explain
+			$('.dropdown-content').fadeOut('normal');  
+		}
+	})
+	$('body').on('touchend', function(e) {
+		e = e || window.event;
+		var obj = e.target || e.srcElement;
+		if($(obj).closest(".dropbtn").length <=0){ // If the clicked area has no id comment, it will hide the explain
+			$('.dropdown-content').fadeOut('normal');  
+		}
+	})
 
-  // android download
-  $('.btn-android').on('click', function() {
-    if (is_weixn()) {
-      var winHeight = $(window).height();
-      $(".weixin-tip").css("height", winHeight);
-      if (window.location.href.indexOf('en') >= 0) {
-        $(".weixin-tip img").attr("src", "../assets/guide-en.png");
-      }
-      $(".weixin-tip").fadeIn();
-    } else {
-      $.ajax({
-        type: "POST",
-        url: "https://api.iwallic.com/api/iwallic",
-        dataType: 'json',
-        data: JSON.stringify({ "method": "fetchIwallicConfig", "params": [] }),
-        success: function(data) {
-          window.location.href = data.result.version_android.url;
-        }
-      })
-    }
-  });
+	// android download
+	$('.btn-android').on('click', function() {
+		if (is_weixn()) {
+			var winHeight = $(window).height();
+			$(".weixin-tip").css("height", winHeight);
+			if (window.location.href.indexOf('en') >= 0) {
+				$(".weixin-tip img").attr("src", "../assets/guide-en.png");
+			}
+			$(".weixin-tip").fadeIn();
+		} else {
+			$.ajax({
+				type: "POST",
+				url: "https://api.iwallic.com/api/iwallic",
+				dataType: 'json',
+				data: JSON.stringify({ "method": "fetchIwallicConfig", "params": [] }),
+				success: function(data) {
+					window.location.href = data.result.version_android.url;
+				}
+			})
+		}
+	});
 
-  $('.weixin-tip button').on('click', function() {
-    $(".weixin-tip").fadeOut();
-  })
+	$('.weixin-tip button').on('click', function() {
+		$(".weixin-tip").fadeOut();
+	})
 
-  // is wechat browser
-  function is_weixn(){    
-    var ua = navigator.userAgent.toLowerCase();    
-    if(ua.match(/MicroMessenger/i)=="micromessenger") {    
-        return true;    
-    } else {    
-        return false;    
-    }    
+	// is wechat browser
+	function is_weixn(){    
+		var ua = navigator.userAgent.toLowerCase();    
+		if(ua.match(/MicroMessenger/i)=="micromessenger") {    
+				return true;    
+		} else {    
+				return false;    
+		}    
 } 
 });
