21d558280a3e787768824996672f9248ce30614a
haiwulei <1021391538@qq.com>
2018-11-19 15:10:14 +0800

    update announcement

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -2,126 +2,112 @@ import * as $ from 'jquery';
 import '../styles/global.scss';
 import '../styles/index.scss';
 
-$(document).ready(function () {
-	var downloadAddress;
-	// apk
-	$.ajax({
-		type: "GET",
-		url: "https://iwallic.forchain.info/client/index/app_version/detail",
-		dataType: 'json',
-		success: function (data) {
-			$('.version').text('v' + data.data.name);
-			downloadAddress = data.data.url;
-		}
-	})
+$(document).ready(function() {
+    var downloadAddress;
+    // apk
+    $.ajax({
+        type: "GET",
+        url: "https://iwallic.forchain.info/client/index/app_version/detail",
+        dataType: 'json',
+        success: function(data) {
+            $('.version').text('v' + data.data.name);
+            downloadAddress = data.data.url;
+        }
+    })
 
-	// article height
-	var articleHeight = window.innerHeight || document.body.clientHeight || document.documentElement.clientHeight;
-	var articleWidth = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
-	articleHeight -= 42;
-	if ($('.left-img').css('display') === 'block') {
-		if (window.location.href.indexOf('en') >= 0) {
-			articleHeight = articleHeight > 905 ? articleHeight : 905;  // 865
-		} else {
-			articleHeight = articleHeight > 753 ? articleHeight : 753;  // 713
-		}
-	} else {
-		if (window.location.href.indexOf('en') >= 0) {
-			articleHeight = articleHeight > 820 ? articleHeight : 820;  // 780
-		} else {
-			articleHeight = articleHeight > 668 ? articleHeight : 668;  // 628
-		}
-	}
-	$("article").css("height", articleHeight);
-	if (articleWidth > 930) {
-		$(".download").hide();
-		$(".pc-download").show();
-	} else {
-		$(".download").show();
-		$(".pc-download").hide();
-	}
+    // article height
+    var articleHeight = window.innerHeight || document.body.clientHeight || document.documentElement.clientHeight;
+    var articleWidth = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+    var rightHeight = $('.right-download').height();
+    articleHeight -= 42;
+    if ($('.left-img').css('display') === 'block') {
+        rightHeight = rightHeight > 665 ? rightHeight : 665;
+    }
+    rightHeight += 30;
+    articleHeight = rightHeight > articleHeight ? rightHeight : articleHeight;
+    $("article").css("height", articleHeight);
+    if (articleWidth > 930) {
+        $(".download").hide();
+        $(".pc-download").show();
+    } else {
+        $(".download").show();
+        $(".pc-download").hide();
+    }
 
-	$(window).on("resize", function () {
-		articleHeight = window.innerHeight || document.body.clientHeight || document.documentElement.clientHeight;
-		articleWidth = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
-		articleHeight -= 42;
-		if ($('.left-img').css('display') === 'block') {
-			if (window.location.href.indexOf('en') >= 0) {
-				articleHeight = articleHeight > 905 ? articleHeight : 905;
-			} else {
-				articleHeight = articleHeight > 753 ? articleHeight : 753;
-			}
-		} else {
-			if (window.location.href.indexOf('en') >= 0) {
-				articleHeight = articleHeight > 820 ? articleHeight : 820;
-			} else {
-				articleHeight = articleHeight > 668 ? articleHeight : 668;
-			}
-		}
-		$("article").css("height", articleHeight);
-		if (articleWidth > 930) {
-			$(".download").hide();
-			$(".pc-download").show();
-		} else {
-			$(".download").show();
-			$(".pc-download").hide();
-		}
-	});
+    $(window).on("resize", function() {
+        articleHeight = window.innerHeight || document.body.clientHeight || document.documentElement.clientHeight;
+        articleWidth = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+        rightHeight = $('.right-download').height();
+        articleHeight -= 42;
+        if ($('.left-img').css('display') === 'block') {
+            rightHeight = rightHeight > 665 ? rightHeight : 665;
+        }
+        rightHeight += 30;
+        articleHeight = rightHeight > articleHeight ? rightHeight : articleHeight;
+        $("article").css("height", articleHeight);
+        if (articleWidth > 930) {
+            $(".download").hide();
+            $(".pc-download").show();
+        } else {
+            $(".download").show();
+            $(".pc-download").hide();
+        }
+    });
 
-	// toggle language
-	if (window.location.href.indexOf('en') >= 0) {
-		$('.dropbtn span').text('English');
-	} else {
-		$('.dropbtn span').text('中文简体');
-	}
-	$('.dropbtn').on('click', function () {
-		$('.dropdown-content').fadeIn('normal');
-	})
-	$('.dropbtn').on('touchstart', function () {
-		$('.dropdown-content').fadeIn('normal');
-	})
+    // toggle language
+    if (window.location.href.indexOf('en') >= 0) {
+        $('.dropbtn span').text('English');
+    } else {
+        $('.dropbtn span').text('中文简体');
+    }
+    $('.dropbtn').on('click', function() {
+        $('.dropdown-content').fadeIn('normal');
+    })
+    $('.dropbtn').on('touchstart', function() {
+        $('.dropdown-content').fadeIn('normal');
+    })
 
-	// toggle language dropcontent
-	$('body').on('click', function (e) {
-		e = e || window.event;
-		var obj = e.target || e.srcElement;
-		if ($(obj).closest(".dropbtn").length <= 0) { // If the clicked area has no id comment, it will hide the explain
-			$('.dropdown-content').fadeOut('normal');
-		}
-	})
-	$('body').on('touchend', function (e) {
-		e = e || window.event;
-		var obj = e.target || e.srcElement;
-		if ($(obj).closest(".dropbtn").length <= 0) { // If the clicked area has no id comment, it will hide the explain
-			$('.dropdown-content').fadeOut('normal');
-		}
-	})
+    // toggle language dropcontent
+    $('body').on('click', function(e) {
+        e = e || window.event;
+        var obj = e.target || e.srcElement;
+        if ($(obj).closest(".dropbtn").length <= 0) { // If the clicked area has no id comment, it will hide the explain
+            $('.dropdown-content').fadeOut('normal');
+        }
+    })
+    $('body').on('touchend', function(e) {
+        e = e || window.event;
+        var obj = e.target || e.srcElement;
+        if ($(obj).closest(".dropbtn").length <= 0) { // If the clicked area has no id comment, it will hide the explain
+            $('.dropdown-content').fadeOut('normal');
+        }
+    })
 
-	// android download
-	$('.btn-android').on('click', function () {
-		if (is_weixn()) {
-			var winHeight = $(window).height();
-			$(".weixin-tip").css("height", winHeight);
-			if (window.location.href.indexOf('en') >= 0) {
-				$(".weixin-tip img").attr("src", "../assets/guide-en.png");
-			}
-			$(".weixin-tip").fadeIn();
-		} else {
-			window.location.href = downloadAddress;
-		}
-	});
+    // android download
+    $('.btn-android').on('click', function() {
+        if (is_weixn()) {
+            var winHeight = $(window).height();
+            $(".weixin-tip").css("height", winHeight);
+            if (window.location.href.indexOf('en') >= 0) {
+                $(".weixin-tip img").attr("src", "../assets/guide-en.png");
+            }
+            $(".weixin-tip").fadeIn();
+        } else {
+            window.location.href = downloadAddress;
+        }
+    });
 
-	$('.weixin-tip button').on('click', function () {
-		$(".weixin-tip").fadeOut();
-	})
+    $('.weixin-tip button').on('click', function() {
+        $(".weixin-tip").fadeOut();
+    })
 
-	// is wechat browser
-	function is_weixn() {
-		var ua = navigator.userAgent.toLowerCase();
-		if (ua.match(/MicroMessenger/i) == "micromessenger") {
-			return true;
-		} else {
-			return false;
-		}
-	}
-});
+    // is wechat browser
+    function is_weixn() {
+        var ua = navigator.userAgent.toLowerCase();
+        if (ua.match(/MicroMessenger/i) == "micromessenger") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+})
